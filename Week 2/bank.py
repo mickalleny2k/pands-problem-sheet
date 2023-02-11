be6ce33a16da074e915d38f101b0bb66e0024550
Michael Allen <mickalleny2k@yahoo.co.uk>
2023-02-11 18:56:01 +0000

    Week 2 task

--- Week 2/bank.py
+++ Week 2/bank.py
@@ -6,3 +6,10 @@ amount1 = input ("enter amount in cents :" )
 amount2 = input ("enter amount in cents :" )
 total = int(amount1) + int(amount2)
 print ("The sum of these is $" + str((total)/100))
+
+# Here is an alternative solution
+# amount1 = input ("enter amount in cents :" )
+# amount2 = input ("enter amount in cents :" )
+# total = int(amount1) + int(amount2)
+# answer = str((total)/100)
+# print (f"The sum of these is ${answer}" )
