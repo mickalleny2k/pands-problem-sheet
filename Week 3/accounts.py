c0de28e549651f588e1910256ffa7fedb70d613f
Michael Allen <mickalleny2k@yahoo.co.uk>
2023-02-15 21:41:24 +0000

    Task for Week 3

--- Week 3/accounts.py
+++ Week 3/accounts.py
@@ -1,4 +1,9 @@
 # Task for Week 3 accounts.py
 # Author : Michael Allen
+import math
 account_number = input ("Please enter an 10 digit account number:")
-print ( "XXXXXX" + account_number[-4:])
+account_length = len(account_number)
+#print (account_length)
+#print ((account_length) - 4)
+#print (((account_length) - 4) * "X")
+print ((((account_length) - 4) * "X")  + account_number[-4:])
