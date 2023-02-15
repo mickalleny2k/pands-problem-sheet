ddb318a573cdf7f91b9bbbfc2eb63ac72dbedf85
Michael Allen <mickalleny2k@yahoo.co.uk>
2023-02-15 20:51:56 +0000

    Task for Week 3

--- Week 3/accounts.py
+++ Week 3/accounts.py
@@ -1,8 +1,4 @@
-# Program for Week 3 accounts.py
+# Task for Week 3 accounts.py
 # Author : Michael Allen
 account_number = input ("Please enter an 10 digit account number:")
-#account_number1 = str(account_number)
-#account_number = "1234567890"
-#print (account_number)
-#account_number1 = account_number[-4,-1]
 print ( "XXXXXX" + account_number[-4:])
