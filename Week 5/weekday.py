ba9b4087e4284d0b18dd8b9515b69988dc5ee274
Michael Allen <mickalleny2k@yahoo.co.uk>
2023-02-28 15:13:45 +0000

    Week 5 Task

--- Week 5/weekday.py
+++ Week 5/weekday.py
@@ -8,22 +8,21 @@ dt = datetime.now()
 x = dt.weekday()
 #print('Day of a week is:', x)
 #print('Datetime is:', dt)
-day_of_week = {"0" : "Oh No. Monday is a weekday", "1" : "Oh No. Tuesday is a weekday", "2" : "Oh No. Wednesday is a weekday",
-           "3" : "Oh No. Thursday is a weekday", "4" : "Oh No. Friday is a weekday", 
-           "Saturday" : "Woohoo. Saturday is the weekend", "6" : "Woohoo. Sunday is the weekend" }
+day_of_week = {"weekday" : "Sorry. Unfortunately it's a weekday.", 
+               "weekend" : "Woohoo. It's the weekend. Party time." }
 
-if x == 0:
-    print (day_of_week["0"])
-elif x == 1 :
-    print (day_of_week["1"])
-elif x == 2 :
-    print (day_of_week["2"])
-elif x == 3 :
-    print (day_of_week["3"])
-elif x == 4 :
-    print (day_of_week["4"])
-elif x == 5 :
-    print (day_of_week["5"])
-elif x == 6 :
-    print (day_of_week["6"])
+if x <= 5:
+    print (day_of_week["weekday"])
+#elif x == 6 :
+#    print (day_of_week["1"])
+#elif x ==  :
+#    print (day_of_week["2"])
+#elif x == 3 :
+#    print (day_of_week["3"])
+#elif x == 4 :
+#    print (day_of_week["4"])
+#elif x == 5 :
+#    print (day_of_week["weekend"])
+else :
+    print (day_of_week["weekend"])
 
