5e7f2c15f27c31592eb25c9a7d03628985cd73a1
Michael Allen <mickalleny2k@yahoo.co.uk>
2023-03-24 14:13:04 +0000

    Week 8 Task

--- Week 8/plottask.py
+++ Week 8/plottask.py
@@ -1,19 +1,26 @@
+# Author : Michael Allen
+# Plotting a function and histogram
+
+# import numpy and matplotlib modules
 import numpy as np
 import matplotlib.pyplot as plt
   
-# Generating some random data
-# for an example
+# Generating random data with 1000 values, mean of 5 and standard deviation of 2 
+# normal distribution
 data = np.random.normal(5, 2, 1000)
   
 # Plotting the histogram.
 plt.hist(data, bins=25, density=True, alpha=0.6, color='b')
-  
+
+#show the plot  
 plt.show()
 
-#plotting the function
+# x and y axis points
 xpoints = np.array(range(0,10))
 ypoints = xpoints**3
 
+#plotting the function
 plt.plot(xpoints,ypoints, 'r')
 
+#show the plot
 plt.show()
