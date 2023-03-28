6a31ecc63a401c12bf2092ce550aab9d9b5903e4
Michael Allen <mickalleny2k@yahoo.co.uk>
2023-03-28 15:19:46 +0100

    Week 8 Task

--- Week 8/plottask.py
+++ Week 8/plottask.py
@@ -12,6 +12,11 @@ data = np.random.normal(5, 2, 1000)
 # Plotting the histogram.
 plt.hist(data, bins=25, density=True, alpha=0.6, color='b')
 
+plt.title("Histogram of Normal Distribution")
+plt.xlabel("1000 values")
+plt.ylabel("Mean 5, standard deviation 2")
+plt.legend()
+
 #show the plot  
 plt.show()
 
@@ -20,7 +25,11 @@ xpoints = np.array(range(0,10))
 ypoints = xpoints**3
 
 #plotting the function
-plt.plot(xpoints,ypoints, 'r')
+plt.plot(xpoints, ypoints, 'r', label = 'y = x cubed')
+plt.title("Function")
+plt.xlabel("0 - 10")
+plt.ylabel("x cubed")
+plt.legend()
 
 #show the plot
 plt.show()
