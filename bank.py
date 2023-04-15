61cfed30234a3091cfd543ebcb945f61c3b1e174
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-15 13:38:26 +0100

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,15 @@
+# Author : Michael Allen
+#Week 2 Assignment
+# Prompt the user and read in two money amounts (in cent)
+
+#amount1 = input ("enter amount in cents :" )
+#amount2 = input ("enter amount in cents :" )
+#total = int(amount1) + int(amount2)
+#print ("The sum of these is $" + str((total)/100))
+
+# Here is an alternative solution
+amount1 = input ("enter amount in cents :" )
+amount2 = input ("enter amount in cents :" )
+total = int(amount1) + int(amount2)
+answer = str((total)/100)
+print (f"The sum of these is ${answer}" )
