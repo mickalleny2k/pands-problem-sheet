27c9a0e4bd52b8ba25fb7bd8e4e604de72609ce6
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-19 14:34:33 +0100

    Delete Week 3 directory
    delete directory as per feedback

--- Week 3/accounts.py
+++
@@ -1,9 +0,0 @@
-# Task for Week 3 accounts.py
-# Author : Michael Allen
-import math
-account_number = input ("Please enter an 10 digit account number:")
-account_length = len(account_number)
-#print (account_length)
-#print ((account_length) - 4)
-#print (((account_length) - 4) * "X")
-print ((((account_length) - 4) * "X")  + account_number[-4:])
