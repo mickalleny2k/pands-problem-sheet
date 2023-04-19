33b3923e2e91f2d84690d075b238f85869fd9d62
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-19 15:53:21 +0100

    Update accounts.py
    I added comments as per feedback

--- accounts.py
+++ accounts.py
@@ -1,9 +1,15 @@
 # Task for Week 3 accounts.py
 # Author : Michael Allen
+
+#import the math module
 import math
+
+#User inputs a 10 digit account number via the command line  
 account_number = input ("Please enter an 10 digit account number:")
+
+#create a variable to measure the length of the number
 account_length = len(account_number)
-#print (account_length)
-#print ((account_length) - 4)
-#print (((account_length) - 4) * "X")
+
+#print the answer to the command line
+#print X's and the last 4 digits
 print ((((account_length) - 4) * "X")  + account_number[-4:])
