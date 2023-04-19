cc2c99d3433ddbab4741746d771224b035785550
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-19 16:56:07 +0100

    Update weekday.py
    I added comments as per feedback

--- weekday.py
+++ weekday.py
@@ -7,11 +7,12 @@ dt = datetime.now()
 # get day of week as an integer
 x = dt.weekday()
 
+#create a dictionary with 2 entries
+#weekday and weekend
 day_of_week = {"weekday" : "Sorry. Unfortunately it's a weekday.", 
                "weekend" : "Woohoo. It's the weekend. Party time." }
 
-if x <= 5:
-    print (day_of_week["weekday"])
-else :
-    print (day_of_week["weekend"])
-
+if x <= 5: #if it's a weekday
+    print (day_of_week["weekday"]) #print Sorry. Unfortunately it's a weekday.
+else : #but if it's the weekend
+    print (day_of_week["weekend"]) #print Woohoo. It's the weekend. Party time.
