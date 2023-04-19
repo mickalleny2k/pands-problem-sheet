8770f161d8e56bdeb83669e2e7f6b7da44fec05a
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-19 13:40:04 +0100

    Update bank.py
    I added comments and research as per your feedback

--- bank.py
+++ bank.py
@@ -8,8 +8,19 @@
 #print ("The sum of these is $" + str((total)/100))
 
 # Here is an alternative solution
+#create two variables for amount1 and amount2
 amount1 = input ("enter amount in cents :" )
 amount2 = input ("enter amount in cents :" )
+
+#create another variable total for the sum
 total = int(amount1) + int(amount2)
+
+#create another variable answer
+#Divide the total by 100 to covert from cents to cents and dollars
 answer = str((total)/100)
+
+#print the answer to the command line
 print (f"The sum of these is ${answer}" )
+
+#research
+#https://realpython.com/python-sum-function/
