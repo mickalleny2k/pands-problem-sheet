5d6f8b55c78084f4878ae097bfb1f61b984708ab
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-19 16:20:49 +0100

    Update collatz.py
    I added comments as per feedback

--- collatz.py
+++ collatz.py
@@ -1,10 +1,18 @@
+# Author : Michael Allen
+#Week 4 Assignment
+
+#import the maths module
 import math
+# user inputs a number. Number variable created.
 number = int(input("enter an integer:"))
+
+#create a while loop
+# loop keeps going while the number is greater than 1
 while number > 1:
-    if (number % 2) == 0:
-        print (str(number) + " is an even number")
-        number = number/2
+    if (number % 2) == 0: #if the number is divided 2 with remainder equal to zero
+        print (str(number) + " is an even number") #print even number
+        number = number/2 # Divide by 2 and update the variable 
          
-    elif (number % 2) != 0:
-        print (str(number) + " is an odd number")
-        number = (number * 3) + 1
+    elif (number % 2) != 0: # else if the number is divided 2 with remainder not equal to zero
+        print (str(number) + " is an odd number") #print odd number
+        number = (number * 3) + 1 # Multiply by 3 and update the variable
