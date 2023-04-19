c21d92189885a74967c87b0dcf1dedf8ad09a34e
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-19 13:26:28 +0100

    Update bank.py
    Added comments as per your feedback

--- Week 2/bank.py
+++ Week 2/bank.py
@@ -2,9 +2,14 @@
 #Week 2 Assignment
 # Prompt the user and read in two money amounts (in cent)
 
+#create two variables for amount1 and amount2
 amount1 = input ("enter amount in cents :" )
 amount2 = input ("enter amount in cents :" )
+
+#create another variable total for the sum
 total = int(amount1) + int(amount2)
+
+#print the sum to the command line
 print ("The sum of these is $" + str((total)/100))
 
 # Here is an alternative solution
