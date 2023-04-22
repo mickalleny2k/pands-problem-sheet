f4fdd629c420b183693db92f63309f19aee76d59
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-22 18:52:12 +0100

    Tidier code
    I made the code tidier.
    There were way too many brackets (((( ))))

--- accounts.py
+++ accounts.py
@@ -1,6 +1,3 @@
-# Task for Week 3 accounts.py
-# Author : Michael Allen
-
 #import the math module
 import math
 
@@ -10,6 +7,9 @@ account_number = input ("Please enter an 10 digit account number:")
 #create a variable to measure the length of the number
 account_length = len(account_number)
 
+#create a variable to measure the number of X's
+number_of_xs = (account_length - 4) * "X"
+
 #print the answer to the command line
 #print X's and the last 4 digits
-print ((((account_length) - 4) * "X")  + account_number[-4:])
+print (number_of_xs + account_number[-4:])
