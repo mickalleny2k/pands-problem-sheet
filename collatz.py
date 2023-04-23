3ff959193307e3abceb50b42a0cecb2345494f8a
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-23 11:43:30 +0100

    output on same line
    I managed to get this program to match the output requested

--- collatz.py
+++ collatz.py
@@ -1,6 +1,5 @@
 # Author : Michael Allen
-#Week 4 Assignment
-
+# Week 4 Assignment
 #import the maths module
 import math
 # user inputs a number. Number variable created.
@@ -10,9 +9,14 @@ number = int(input("enter an integer:"))
 # loop keeps going while the number is greater than 1
 while number > 1:
     if (number % 2) == 0: #if the number is divided 2 with remainder equal to zero
-        print (str(number) + " is an even number") #print even number
-        number = number/2 # Divide by 2 and update the variable 
+        print (str(int(number)), end=' ') #print even number
+        number = number/2 # Divide by 2 and update the variable       
          
     elif (number % 2) != 0: # else if the number is divided 2 with remainder not equal to zero
-        print (str(number) + " is an odd number") #print odd number
+        print (str(int(number)), end=' ') #print odd number
         number = (number * 3) + 1 # Multiply by 3 and update the variable
+
+#Research
+#https://stackoverflow.com/questions/33508034/making-a-collatz-program-automate-the-boring-stuff
+#https://stackoverflow.com/questions/33905032/how-to-print-on-the-same-line-in-python
+        
