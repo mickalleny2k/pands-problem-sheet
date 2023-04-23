5e5976681409a49f05b3497ddd5dea544b4cb5cf
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-23 14:20:43 +0100

    prettier plot as per feedback
    I made the plot prettier as per feedback
    
    Research :
    https://matplotlib.org/stable/gallery/text_labels_and_annotations/text_fontdict.html

--- plottask.py
+++ plottask.py
@@ -4,17 +4,30 @@
 # import numpy and matplotlib modules
 import numpy as np
 import matplotlib.pyplot as plt
+
+font_for_histogram = {'family': 'cambria',
+        'color':  'darkblue',
+        'weight': 'normal',
+        'size': 14,
+        }
+
+font_for_plot = {'family': 'calibri',
+        'color':  'darkred',
+        'weight': 'bold',
+        'size': 20,
+        }
   
 # Generating random data with 1000 values, mean of 5 and standard deviation of 2 
 # normal distribution
 data = np.random.normal(5, 2, 1000)
   
 # Plotting the histogram.
+plt.style.use("seaborn-darkgrid")
 plt.hist(data, bins=25, density=True, alpha=0.6, color='b')
 
-plt.title("Histogram of Normal Distribution")
-plt.xlabel("1000 values")
-plt.ylabel("Mean 5, standard deviation 2")
+plt.title("Normal Distribution", color='blue', fontdict=font_for_histogram)
+plt.xlabel("Mean 5, St Dev 2", color='blue', fontdict=font_for_histogram)
+plt.ylabel("1000 Random values", color='blue', fontdict=font_for_histogram)
 plt.legend()
 
 #show the plot  
@@ -25,10 +38,11 @@ xpoints = np.array(range(0,10))
 ypoints = xpoints**3
 
 #plotting the function
+plt.style.use("seaborn-darkgrid")
 plt.plot(xpoints, ypoints, 'r', label = 'y = x cubed')
-plt.title("Function")
-plt.xlabel("0 - 10")
-plt.ylabel("x cubed")
+plt.title("Function", color='red', fontdict=font_for_plot)
+plt.xlabel("0 - 10", color='red', fontdict=font_for_plot)
+plt.ylabel("x-cubed", color='red', fontdict=font_for_plot)
 plt.legend()
 
 #show the plot
