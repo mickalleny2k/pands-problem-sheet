b42e8d902915269ddb5556f578a6a5f01b585b7f
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-23 12:22:47 +0100

    my own function as per feedback
    my own function that takes in a number and returns a value

--- sqrroot.py
+++ sqrroot.py
@@ -3,10 +3,10 @@
 # Calculate the square root of a number using Newtons method
 
 # user inputs a number
-n = input("Please input a number : ")
+n = input("Please input a positive integer : ")
 
 # create a function using def 
-def square_root(n, l) :
+def sqrt(n) :
  
     # Set x equal to n at the start
     x = n
@@ -14,21 +14,23 @@ def square_root(n, l) :
     # To count the number of loop iterations
     count = 0
  
-    while (1) :
+    while (n > 1) :
         count += 1
  
         # Newton's formula
         root = 0.5 * (x + (n / x))
  
         # Check if answer is less than 1
-        if (abs(root - x) < l) :
+        if (abs(root - x) < 1) :
             break
             # break out of the loop if answer is less than 1
 
         # Update root and perform another iteration if answer is greater than 1 
         x = root
  
-    return root
-  
-print(f"The square root of {n} is {square_root(int(n), 1)}")
+    return float(root)
+
+print(f"The square root of {n} is approx. {sqrt(float(n)):.1f}")
+
+
  
