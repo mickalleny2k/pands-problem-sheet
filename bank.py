456a1fd493bb56bcf4a68a43bba352ab0c4b6afa
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-24 22:45:47 +0100

    re.sub() function to replace dollars with cents
    change cents to dollars without dividing by 100 as per feedback
    Use re.sub() function to replace dollars with cents
    https://stackoverflow.com/questions/66662279/python-shift-decimal-point-in-a-string-number

--- bank.py
+++ bank.py
@@ -1,36 +1,25 @@
 # Author : Michael Allen
 #Week 2 Assignment
 # Prompt the user and read in two money amounts (in cent)
-#import re
-#import decimal
-#amount1 = input ("enter amount in cents :" )
-#amount2 = input ("enter amount in cents :" )
-#total = int(amount1) + int(amount2)
-#print ("The sum of these is $" + str((total)/100))
+import re
+import decimal
+import math
 
-# Here is an alternative solution
 #create two variables for amount1 and amount2
 amount1 = input ("enter amount in cents :" )
 amount2 = input ("enter amount in cents :" )
 
-#create another variable total for the sum
-total = float(amount1) + float(amount2)
-
-#create another variable
-hundred = 100
-#create another variable answer
-#Divide the total by divide  to covert from cents to cents and dollars
-answer = total/hundred
-
-#print the answer to the command line
-#You can use "f-strings" (f for "formatted string literals"), the short format style from Python v3.6 on
-#https://stackoverflow.com/questions/2389846/python-decimals-format
-print (f"The sum of these is ${answer:.2f}" )
+#Substitute function in re module
+result1 = re.sub(r'(..)(\.|$)', r'.\1', amount1)
+#print(result1)
+#change cents to dollars
+result2 = re.sub(r'(..)(\.|$)', r'.\1', amount2)
+#print(result2)
+total = float(result1) + float(result2)
+#format the answer and print
+print(f"The sum of these is ${total:.2f}")
 
 #research
 #https://realpython.com/python-sum-function/
-
-#left_shift = 2
-#split = total.split(".")
-#"".join([split[0][:-left_shift], ".", split[0][-left_shift:], split[1]])
-#print(split)
+#https://stackoverflow.com/questions/66662279/python-shift-decimal-point-in-a-string-number
+#https://pythonexamples.org/python-re-sub/#:~:text=Python%20re.sub%28%29%20Function.%20re.sub%28%29%20function%20replaces%20one%20or,function%20is.%20re.sub%28pattern%2C%20repl%2C%20string%2C%20count%3D0%2C%20flags%3D0%29%20where
