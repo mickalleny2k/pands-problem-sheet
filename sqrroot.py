4adb0087acd2d575ec26b40f54ed664c7fbde806
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-25 20:17:42 +0100

    If statement
    If the user enters zero or a negative number, the user is asked again to enter a positive integer.
    Everybody deserves a second chance afterall.

--- sqrroot.py
+++ sqrroot.py
@@ -4,32 +4,32 @@
 
 # user inputs a number
 n = input("Please input a positive integer : ")
+if int(n) <= 0:
+    #print (" Please enter a positive integer")
+    n = input("LISTEN TO ME -))) I SAID please input a positive integer : ")
+elif int(n) > 0:
+    sqrt(n)
 
-# create a function using def 
+# write a function that takes in a number and returns a value 
 def sqrt(n) :
- 
     # Set x equal to n at the start
     x = n
- 
     # To count the number of loop iterations
     count = 0
- 
+    # the loop continues as long as n is greater than 1
     while (n > 1) :
         count += 1
- 
         # Newton's formula
         root = 0.5 * (x + (n / x))
- 
         # Check if answer is less than 1
         if (abs(root - x) < 1) :
             break
             # break out of the loop if answer is less than 1
-
         # Update root and perform another iteration if answer is greater than 1 
         x = root
- 
     return float(root)
 
+#print the square root. Format the answer. One decimal point.
 print(f"The square root of {n} is approx. {sqrt(float(n)):.1f}")
 
 
